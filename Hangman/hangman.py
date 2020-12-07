270dc69822bd61c9df9d2de7cf7e8d62903ae75e
Rashmitha <rashmitha.prabhu16@gmail.com>
2020-12-07 20:52:42 +0530

    Hangman

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -1,62 +1,82 @@
+from os import wait
 import random
 import hangman_art
 import hangman_words
+import subprocess as sp
+
+def clear():
+    tmp = sp.call('clear', shell=True)
+    print(hangman_art.logo+"\n\n")
 
 word_list = hangman_words.word_list
-chosen_word = random.choice(word_list)
-word_length = len(chosen_word)
 
-end_of_game = False
-lives = 6
+while True:
+    tmp = sp.call('clear', shell=True)
+    print(hangman_art.logo+"\n\n")
+    chosen_word = random.choice(word_list)
+    word_length = len(chosen_word)
 
-print(hangman_art.logo+"\n\n")
+    end_of_game = False
+    lives = 6
 
-display = []
-entered=[]
+    display = []
+    entered=[]
 
-for _ in range(word_length):
-    display += "_"
+    for _ in range(word_length):
+        display += "_"
 
-n = round(word_length/4)
+    n=word_length
 
-while n >= 0:
-    i = random.randint(0,word_length-1)
-    if chosen_word[i] not in display:
-        letter = chosen_word[i]
-        for i in range(word_length):
-            if chosen_word[i]==letter:
-                display[i]=letter
-                n-=1
+    while n > word_length-2:
+        i = random.randint(0,word_length-1)
+        if chosen_word[i] not in display:
+            letter = chosen_word[i]
+            for i in range(word_length):
+                if chosen_word[i]==letter:
+                    display[i]=letter
+                    n-=1
 
-print(f"{' '.join(display)}") 
+    while not end_of_game:
+        print(f"{' '.join(display)}") 
+        print(hangman_art.stages[lives])
+        print(f"Already entered : {entered}")
+        print(f"Lives Remaining : {lives}")
+        guess = input("\nGuess a letter: ").lower()	
 
-while not end_of_game:
-    guess = input("\nGuess a letter: ").lower()	
+        if guess in entered:
+            clear()
+            continue
 
-    if guess in entered:
-        print(f"\nAlready Entered. Choose again\n")
-        continue
+        else:
+            entered+=guess
 
-    else:
-        entered+=guess
+        for position in range(word_length):
+            letter = chosen_word[position]
+            if letter == guess:
+                display[position] = letter
 
-    for position in range(word_length):
-        letter = chosen_word[position]
-        if letter == guess:
-            display[position] = letter
+        if guess not in chosen_word:
+            lives -= 1
 
-    if guess not in chosen_word:
-        lives -= 1
-        print(f"\nUh-Oh! {guess} doesnt occur in the word. You now have {lives} lives left")
-        if lives == 0:
+            if lives == 0:
+                end_of_game = True
+                clear()
+                print(f"{' '.join(display)}") 
+                print(hangman_art.stages[lives])
+                print(f"\nThe word was {chosen_word}")
+                print("\nYou Lose")
+                break
+
+        if "_" not in display:
             end_of_game = True
-            print(f"\n\nThe word was {chosen_word}")
-            print("\nYou Lose")          
+            clear()
+            print(f"{' '.join(display)}") 
+            print(hangman_art.stages[lives])
+            print("\nYou win.")
+        else:
+            clear()
 
-    print(f"{' '.join(display)}")    
-
-    print(hangman_art.stages[lives])
-
-    if "_" not in display:
-        end_of_game = True
-        print("\n\nYou win.")
+    choice = input("\nTry Again? (y/n) : ")
+    if choice.lower() == 'n':
+        print("See ya Again!")
+        exit()
