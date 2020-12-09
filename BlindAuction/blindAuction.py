826536da551455472d9c41695e8c97dcf2dc70b2
Rashmitha <rashmitha.prabhu16@gmail.com>
2020-12-09 20:05:14 +0530

    BlindAuction

--- BlindAuction/blindAuction.py
+++ BlindAuction/blindAuction.py
@@ -4,26 +4,26 @@ import subprocess as sp
 print(art.logo)
 print("WELCOME TO SECRET AUCTION PROGRAM")
 
-bidders={}
+bidders = {}
 proceed = True
 
 while proceed:
     name = input("\nEnter your name : \t")
     bid = int(input("Enter your bid: \t$"))
 
-    bidders[name]=bid;
+    bidders[name] = bid
 
     flag = input("Any other bidders? (Y/N)\t: ")
-    if flag.lower()=="n":
+    if flag.lower() == "n":
         proceed = False
     tmp = sp.call('clear', shell=True)
 
-max = 0
-name=""
+maximum = 0
+name = ""
 
 for key in bidders:
-    if bidders[key]>max:
-        max = bidders[key]
+    if bidders[key] > maximum:
+        maximum = bidders[key]
         name = key
 
-print(f"\n{name} won the bid at ${max}\n")
+print(f"\n{name} won the bid at ${maximum}\n")
--- GuessTheNumber/tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
-n = int(input("Make a guess : "))
-    if n>num:
-        print("Too HIGH")
-    elif n<num:
-        print("Too LOW")
-    else:
-        print("You guessed it! Congratulations!")
-        exit()
