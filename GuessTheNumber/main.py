bde8cda7d2721f0e92afd2a8b5af68ced029df72
Rashmitha <rashmitha.prabhu16@gmail.com>
2020-12-20 18:32:09 +0530

    GuessTheNumber

--- GuessTheNumber/main.py
+++ GuessTheNumber/main.py
@@ -2,18 +2,20 @@ import art
 import random
 
 print(art.logo)
-num = random.randint(1,100)
+num = random.randint(1, 100)
 
-def game(guess):
-    print(f"\nREMAINING LIVES : {guess}\n")
+
+def game(g):
+    print(f"\nREMAINING LIVES : {g}\n")
     n = int(input("Make a guess : "))
-    if n>num:
+    if n > num:
         print("Too HIGH")
-    elif n<num:
+    elif n < num:
         print("Too LOW")
     else:
         print("You guessed it! Congratulations!")
         exit()
+
 
 diff = input("Choose Difficulty : \nEASY (10 chances) \t\t HARD(10 chances) : ")
 
@@ -22,15 +24,15 @@ print("Can You guess it?\n")
 
 if diff.lower() == "easy":
     guess = 10
-    while guess>0:
+    while guess > 0:
         game(guess)
-        guess-=1
+        guess -= 1
     print(f"\nYou ran out of chances.\nThe number on my mind was {num}\nBetter luck next time!")
 
 if diff.lower() == "hard":
     guess = 5
-    while guess>=0:
+    while guess >= 0:
         game(guess)
-        guess-=1
+        guess -= 1
     print(f"\nYou ran out of chances.\nThe number on my mind was {num}\nBetter luck next time!")
 
