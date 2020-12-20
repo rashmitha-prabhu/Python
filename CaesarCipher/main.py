bbaa1fc87f88e0122d51059d5c6338ad864aa625
Rashmitha <rashmitha.prabhu16@gmail.com>
2020-12-20 18:26:27 +0530

    CaesarCipher

--- CaesarCipher/main.py
+++ CaesarCipher/main.py
@@ -1,8 +1,10 @@
 import art
 
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+            'v', 'w', 'x', 'y', 'z']
 
-def caesar(direction,text,shift):
+
+def caesar(direction, text, shift):
     msg = ""
     for letter in text:
         if letter not in alphabet:
@@ -11,18 +13,21 @@ def caesar(direction,text,shift):
         pos = alphabet.index(letter)
         n = len(alphabet)
         if direction.lower() == "encode":
-            msg += alphabet[(pos+shift)%n]
+            msg += alphabet[(pos+shift) % n]
         elif direction.lower() == "decode":
-            msg += alphabet[abs(pos+n-shift)%n]
+            msg += alphabet[abs(pos+n-shift) % n]
     print(f"The {direction.lower()}d text is {msg}")
 
+
 print(art.logo)
+
 
 def inputs():
     direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
     text = input("Type your message:\n").lower()
     shift = int(input("Type the shift number:\n"))
-    caesar(direction,text,shift)
+    caesar(direction, text, shift)
+
 
 def check(restart):
     if restart.lower() == "yes":
@@ -30,5 +35,6 @@ def check(restart):
         check(input("\n\nType 'yes' to go again, else type 'no':\n"))
     else:
         print("\n\nGood bye\n")
+
 
 check("yes")
