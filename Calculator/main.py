73ec6ec7f9a5de0de029d869362c96949eddc10b
Rashmitha <rashmitha.prabhu16@gmail.com>
2020-12-20 18:28:47 +0530

    Calculator

--- Calculator/main.py
+++ Calculator/main.py
@@ -2,30 +2,36 @@ import art
 
 print(art.logo)
 
-def add(n1,n2):
+
+def add(n1, n2):
     return n1+n2
 
-def subtract(n1,n2):
+
+def subtract(n1, n2):
     return n1-n2
 
-def multiply(n1,n2):
+
+def multiply(n1, n2):
     return n1*n2
 
-def divide(n1,n2):
-    if n2==0:
+
+def divide(n1, n2):
+    if n2 == 0:
         return "Divide by zero error"
     return n1/n2
 
+
 operations = {
-    "+":add,
-    "-":subtract,
-    "*":multiply,
-    "/":divide,
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide,
 }
 
 print("Operations available: ")
-for keys in operations : 
+for keys in operations:
     print(keys)
+
 
 def calculator():
     num1 = float(input("What's the first number? : "))
@@ -34,7 +40,7 @@ def calculator():
         op = input("Operation : ")
         num2 = float(input("What's the next number? : "))
         function = operations[op]
-        ans = round(function(num1,num2),2)
+        ans = round(function(num1, num2), 2)
         print(f"\n{num1} {op} {num2} = {ans}\n")
         
         ch = input(f"Type 'y' to continue calculating with {ans}, 'n' to restart and 'x' to exit: ")
@@ -43,5 +49,6 @@ def calculator():
             num1 = ans
         elif ch == 'n':
             calculator()
+
 
 calculator()
