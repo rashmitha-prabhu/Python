2ec1cd6c95b7092b7ead25642d40ce78408d3671
Rashmitha <rashmitha.prabhu16@gmail.com>
2021-01-04 10:14:34 +0530

    NATO

--- NATO/main.py
+++ NATO/main.py
@@ -3,7 +3,16 @@ import pandas
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
 nato_phonetic = {rows.letter: rows.code for (index, rows) in data.iterrows()}
 
-name = input("Enter a word : ").upper()
-phonetic = [nato_phonetic[letter] for letter in name]
 
-print(phonetic)
+def get_name():
+    name = input("Enter a word : ").upper()
+    try:
+        phonetic = [nato_phonetic[letter] for letter in name]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        get_name()
+    else:
+        print(phonetic)
+
+
+get_name()
